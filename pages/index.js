0f963b39a0233c58bfb5b6c292df96c9adbf3527
Pedro Vieira <c00123@comunix.br>
2021-05-20 18:16:26 -0300

    outra alteração

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          João Pé de Feijão
+          Pedro doidao
         </h1>
 
         <p className={styles.description}>
