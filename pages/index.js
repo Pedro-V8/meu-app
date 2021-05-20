d7107667ea6e4e82331c99080c6e5549bd5c1f1b
Pedro Vieira <c00123@comunix.br>
2021-05-20 18:14:06 -0300

    Alteração

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          João Pé de Feijão
         </h1>
 
         <p className={styles.description}>
