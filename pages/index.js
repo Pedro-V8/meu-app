bd39a6d889af37e04a96526699a325cf9ce01330
Pedro-V8 <pedrophdv@outlook.com>
2021-05-21 10:11:56 -0300

    Alterações

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Pedro doidao
+          Oiii
         </h1>
 
         <p className={styles.description}>
