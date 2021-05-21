965bcdaf068e6425a6247c00cfc373fc2ea4ca9f
Pedro-V8 <pedrophdv@outlook.com>
2021-05-21 10:23:05 -0300

    Alterações com o prisma

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Oiii
+          Estou com Prisma =)
         </h1>
 
         <p className={styles.description}>
